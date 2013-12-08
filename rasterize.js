ce22a0b6c17daddd24bdfa3c5e02196abe170dfb
Juho Vepsalainen <bebraw@gmail.com>
2013-12-08 21:01:19 +0200

    Add support for `zoomFactor`
    Note that I also fixed windowX and windowY parameters while at it. I was
    using the wrong property names at webshot.

--- rasterize.js
+++ rasterize.js
@@ -10,14 +10,16 @@ function main() {
     program.version(require('./package.json').version).
         option('-s --site <url>', 'site to capture').
         option('-o --output <filename>', 'output name, defaults to shot.png').
-        option('-wx --windowX <pixels>', 'window x pixels, defaults to 1024').
-        option('-wy --windowY <pixels>', 'window y pixels, defaults to 768').
+        option('-wx --windowX <pixels>', 'window x pixels, defaults to 1024', parseInt).
+        option('-wy --windowY <pixels>', 'window y pixels, defaults to 768', parseInt).
+        option('-z --zoomFactor <factor as float>', 'zoom factor, defaults to 1.0', parseFloat).
         parse(process.argv);
 
     if(!program.site) return console.warn('Missing site to capture!');
     if(!program.output) program.output = 'shot.png';
     if(!program.windowX) program.windowX = 1024;
     if(!program.windowY) program.windowY = 768;
+    if(!program.zoomFactor) program.zoomFactor = 1.0;
 
     capture(program);
 }
