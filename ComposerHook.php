0b77d9700d24d81e853836de58f6c2ac3c53dcd3
lividgreen <lividgreen@gmail.com>
2013-12-09 02:17:30 +0200

    composer hook 2

--- ComposerHook.php
+++ ComposerHook.php
@@ -7,6 +7,7 @@ class ComposerHook {
         if (!$path = shell_exec('which npm')) {
             die("Can't find 'npm' on your system");
         }
+        echo "NPM: $path\n";
         shell_exec('cd '.escapeshellarg(__DIR__).' && ' . $path . ' update');
     }
 }
