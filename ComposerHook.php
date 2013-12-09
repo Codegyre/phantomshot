5316e910c49602efaf7bb3adb9bd9c7bba360478
lividgreen <lividgreen@gmail.com>
2013-12-09 03:50:46 +0200

    composer hook 3

--- ComposerHook.php
+++ ComposerHook.php
@@ -2,12 +2,48 @@
 
 namespace Codegyre\PhantomShot;
 
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+use Symfony\Component\Process\ExecutableFinder;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
+
 class ComposerHook {
-    public static function hook() {
-        if (!$path = shell_exec('which npm')) {
-            die("Can't find 'npm' on your system");
+    public static function hook(Event $event) {
+        switch ($event->getName()) {
+            case ScriptEvents::POST_INSTALL_CMD:
+                $command = 'install';
+                break;
+            case ScriptEvents::POST_UPDATE_CMD:
+                $command = 'update';
+                break;
+            default:
+                return;
+
         }
-        echo "NPM: $path\n";
-        shell_exec('cd '.escapeshellarg(__DIR__).' && ' . $path . ' update');
-    }
+
+        $executableFinder = new ExecutableFinder;
+        $npmPath = $executableFinder->find('npm');
+        if ($npmPath === null) {
+            throw new \RuntimeException('Unable to locate npm executable.');
+        }
+
+        $processBuilder = new ProcessBuilder(array(
+            $npmPath,
+            $command,
+        ));
+        $processBuilder->setWorkingDirectory(__DIR__);
+        $npmProcess = $processBuilder->getProcess();
+
+        $io = $event->getIO();
+
+        $npmProcess->run(function ($type, $buffer) use ($io) {
+            if (Process::ERR === $type) {
+                throw new \RuntimeException($buffer);
+            } else {
+                $io->write($buffer, false);
+            }
+        });
+   }
+
 }
